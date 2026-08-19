da28b764f923fb50c4333c4ebaf086e2c43cc0aa
Sajib Kumar Roy <sajibr917@gmail.com>
2026-08-19 18:25:40 +0600

    Backend

--- client/app/(auth)/login/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { AuthForm } from "@/components/auth/auth-form";
-
-const LoginPage = () => {
-    return <AuthForm mode="login" />;
-};
-
-export default LoginPage;
--- client/app/(auth)/register/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { AuthForm } from "@/components/auth/auth-form";
-
-const RegisterPage = () => {
-    return <AuthForm mode="register" />;
-};
-
-export default RegisterPage;
